f669b4915254d0491d476af5984b62fd62bc4a7a
Dylan Balagtas <dylansatgalab@gmail.com>
2021-05-06 02:13:13 +0800

    Styles Hero
    - needs padding per form
    - needs to change meta data

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,26 +1,57 @@
 <template>
     <div>
-      <form action="">
         <h1>Australia's most trusted trades provider</h1>
+      <form action="">
         <div class="form-example">
-          <label for="name">What trade do you need?</label>
+          <label for="name">What trade do you need?</label><br>
           <input type="text" name="name" id="name" required />
         </div>
         <div class="form-example">
-          <label for="name">Postcode or Suburb</label>
+          <label for="name">Postcode or Suburb</label><br>
           <input type="text" name="name" id="name" required />
         </div>
-        <input type="button" value="post a job" />
+        <input type="submit" value="post a job" />
       </form>
     </div>
 </template>
 
 <script>
 export default {
-
 }
 </script>
 
-<style>
+<style scoped>
+*{
+    --main-color: #475f7b;
+    --sec-color: #ff5b5c;
+    font-family: Rubik,Helvetica,Arial,serif;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+}
+h1, label {
+    color: var(--main-color);
+}
+h1 {
+    font-size: 4rem;
+}
+input[type="text"]{
+    width: 464px;
+    height:43px;
+}
+input[type="submit"]{
+    color: white;
+    background-color: var(--sec-color);
+    border: none;
+    border-radius: 5px;
+    width: 341px;
+    height:60px;
+    
+}
 
+form {
+    justify-content: space-evenly;
+    align-items: center;
+    align-content: center;
+}
 </style>
