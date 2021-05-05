3517070fad86fe4b310653102174048d374c3284
Dylan Balagtas <dylansatgalab@gmail.com>
2021-05-06 01:03:20 +0800

    Adds form with content

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
-  <div id="app">
+
+<div>
+<header></header>
+  <div>
+    <form action="">
+ <h1>Australia's most trusted trades provider</h1>
+  <div class="form-example">
+    <label for="name">What trade do you need?</label>
+    <input type="text" name="name" id="name" required>
+  </div>
+  <div class="form-example">
+    <label for="name">Postcode or Suburb</label>
+    <input type="text" name="name" id="name" required>
+  </div>
+    <input type="button" value="post a job">
+    </form>
+  </div>
+  <footer></footer>
+</div>
+
+  <!-- <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  </div> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
